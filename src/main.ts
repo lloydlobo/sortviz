d67e1096d07e1e39e6af2affebcc253541176d21
lloydlobo <lloyd@lloydlobo.com>
2022-07-05 19:15:15 +0530

    style: add brand colors

--- src/main.ts
+++ src/main.ts
@@ -90,18 +90,23 @@ const bubbleSort = async (array) => {
         // reset color of bars visited during the previous iteration
         for (let k = 0; k < bars.length; k += 1) {
           if (k !== j && k !== j + 1) {
-            (bars[k] as HTMLDivElement).style.backgroundColor = "teal";
+            (
+              bars[k] as HTMLDivElement
+            ).style.backgroundColor = `var(--clr-secondary)`;
           }
         }
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
 
-        (bars[j] as HTMLDivElement).style.backgroundColor = "aquamarine";
+        (
+          bars[j] as HTMLDivElement
+        ).style.backgroundColor = `var(--clr-tertiary)`;
         (bars[j] as HTMLDivElement).style.height =
           (array[j] * factorHeight) / 16 + "rem";
 
-        (bars[j + 1] as HTMLDivElement).style.backgroundColor = "aqua";
+        (bars[j + 1] as HTMLDivElement).style.backgroundColor =
+          "var(--clr-primary)";
         (bars[j + 1] as HTMLDivElement).style.height =
           (array[j + 1] * factorHeight) / 16 + "rem";
 
