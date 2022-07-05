cf2248d6a618cd4776e83a5529a02f80231891ad
lloydlobo <lloyd@lloydlobo.com>
2022-07-05 11:31:05 +0530

    feat: add footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const yearCurrent = new Date().getFullYear();
+  const author = "Lloyd Lobo";
+  const url = "https://github.com/lloydlobo/";
+</script>
+
+<footer class="footer flex flex-col justify-center">
+  <p class="footer-author text-sm">
+    Made with <span class="text-rose-500">♥</span> by
+    <span class="author"><a href={url}>{author}</a></span>
+  </p>
+
+  <p class="copyright text-xs">&copy;&nbsp;{yearCurrent}</p>
+</footer>
