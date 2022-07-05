59321c8582afce2475220763f32433cfb3904e92
lloydlobo <lloyd@lloydlobo.com>
2022-07-05 17:46:37 +0530

    feat: add bubbleSort algorithm

--- src/main.ts
+++ src/main.ts
@@ -79,3 +79,47 @@ btnRandomizeArray.addEventListener("click", () => {
   barsContainer.innerHTML = "";
   renderBars(arrayNotSorted);
 });
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const bubbleSort = async (array) => {
+  let bars = document.getElementsByClassName("bar");
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length - i - 1; j += 1) {
+      if (array[j] > array[j + 1]) {
+        // reset color of bars visited during the previous iteration
+        for (let k = 0; k < bars.length; k += 1) {
+          if (k !== j && k !== j + 1) {
+            (bars[k] as HTMLDivElement).style.backgroundColor = "teal";
+          }
+        }
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+
+        (bars[j] as HTMLDivElement).style.backgroundColor = "aquamarine";
+        (bars[j] as HTMLDivElement).style.height =
+          (array[j] * factorHeight) / 16 + "rem";
+
+        (bars[j + 1] as HTMLDivElement).style.backgroundColor = "aqua";
+        (bars[j + 1] as HTMLDivElement).style.height =
+          (array[j + 1] * factorHeight) / 16 + "rem";
+
+        await sleep(factorSpeed);
+      }
+    }
+    await sleep(factorSpeed);
+  }
+
+  console.log("sort");
+  return array;
+};
+
+btnSort.addEventListener("click", () => {
+  switch (algorithmSelected) {
+    case "bubble": {
+      bubbleSort(arrayNotSorted);
+      break;
+    }
+  }
+});
